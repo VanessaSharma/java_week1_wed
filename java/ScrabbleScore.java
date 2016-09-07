ff7d44f00d5170b623245f4bf83e853bb99846e5
Shelby Clayton and Vanessa Palacios Sharma <shelby@>
2016-09-07 14:01:56 -0700

    Add basic code for scrabble score keeper.

---
+++ java/ScrabbleScore.java
@@ -0,0 +1,20 @@
+import java.io.Console;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class ScrabbleScore {
+  public static void main(String[] args) {
+    Console myConsole = System.console();
+    String letterList = "aeioulnrstdgbcmpfhvwykjxqz";
+    Integer[] valueOfLetters = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 8, 8, 10, 10};
+
+
+    System.out.println( "What is your word?");
+    String userAnswer = myConsole.readLine();
+    String[] userLetters = userAnswer.split(" ");
+
+    System.out.println( userLetters);
+}
+}
